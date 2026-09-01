80bbc3a1ae3250e3d13208a833b42cb86be07f11
Hassnmahad <hassanmahad07@gmail.com>
2026-08-31 20:48:24 -0700

    Execise 3 ayaan kuso daray

---
+++ Exercise 3/src/App.jsx
@@ -0,0 +1,25 @@
+import UseCard from "./UseCard";
+
+
+function App(){
+
+  return(
+    <>
+    <UseCard 
+    Name = "Hassan"
+    Email = "hassanmahad07@gmail.com"
+    />
+
+    <UseCard 
+    Name = "Cali"
+    Email = "Cali@gmail.com"
+    />
+
+    <UseCard 
+    Name = "Yuusuf"
+    Email = "Yuusuf@gmail.com"
+    />
+    </>
+  );
+}
+export default App; 
---
+++ Exercise 3/src/UseCard.jsx
@@ -0,0 +1,13 @@
+
+const UseCard = (props) => {
+    return(
+         <>
+    <p>Hello , {props.Name} </p>
+    <p>Hello , {props.Email} </p>
+    <p>_______________________</p>
+    </>
+    )
+   
+}
+
+export default UseCard; 
