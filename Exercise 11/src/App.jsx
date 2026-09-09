303bf008ebed220d210c1834c592d0f4129c6149
Hassnmahad <hassanmahad07@gmail.com>
2026-09-09 17:25:33 +0300

    Execise 11 ayaan kuso daray

---
+++ Exercise 11/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    if (username && password) {
+      setIsLogin(true);
+    }
+  };   
+
+
+  const handleLogOut = () => {
+    setUsername("");
+    setPassword("");
+    setIsLogin(false);
+  };
+  if (isLogin) {
+    return (
+      <div>
+        <h1>Welcome , {username}</h1>
+        <button onClick={handleLogOut}>LogOut</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <label>Username : 
+        <input
+          type="text"
+          placeholder="Enter Username"
+          required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        /></label>
+        <br />
+        <label>Password : 
+        <input
+          type="password"
+          placeholder="Enter Password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        /></label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
---
+++ Exercise 11/src/WelcomePage.jsx
@@ -0,0 +1,10 @@
+
+const WelcomePage = (username)=>{
+return(
+    <>
+    <h1>Welcome , {username} </h1>
+    </>
+)
+}
+
+export default WelcomePage;
